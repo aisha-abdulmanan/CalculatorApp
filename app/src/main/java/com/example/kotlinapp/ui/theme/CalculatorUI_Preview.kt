607b731f3dd0756d150ae80005fc19ab09b34e12
aisha-abdulmanan <sittieaisha514@gmail.com>
2024-12-28 04:19:17 +0800

    Few Modifications on files (.gitignore & CalculatorUI_Preview.kt)

--- app/src/main/java/com/example/kotlinapp/ui/theme/CalculatorUI_Preview.kt
+++ app/src/main/java/com/example/kotlinapp/ui/theme/CalculatorUI_Preview.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.kotlinapp.Calculator
 import com.example.kotlinapp.R
 
 
@@ -94,7 +93,7 @@ fun CalculatorUI_Preview(){
 fun CalculatorButtonS(btn : String, onClick : ()-> Unit){
     Box(modifier = Modifier.padding(4.dp)) {
         FloatingActionButton(
-            onClick = { },
+            onClick = onClick,
             modifier = Modifier.size(65.dp),
             containerColor = getColorS(btn)
         ) {
